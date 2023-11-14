7f4d40da4395e487cde565180363db42a59214de
Akramah Faizi <akramahahmadfaizi@gmail.com>
2023-11-14 23:33:50 +0530

    till 13

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,32 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.purple[200],
       ),
-      body: const Center(
-        child: Image(image: NetworkImage('https://i.pinimg.com/originals/b9/75/29/b97529d497387509165c215bf8fb9222.jpg'),
-          fit: BoxFit.cover,)
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Hey Ninjas!', style: TextStyle(backgroundColor: Colors.cyan),),
+          ElevatedButton(onPressed: (){}, child: Icon(Icons.add)),
+          Container(
+            color: Colors.blue,
+            child: Text('AGAIN'),
+            padding: EdgeInsets.all(20.0),
+          ),
+          Column(
+            children: [
+              Text('Check the length', style: TextStyle(backgroundColor: Colors.cyan),),
+              IconButton(onPressed: (){}, icon: Icon(Icons.add)),
+              ElevatedButton(onPressed: (){}, child: Icon(Icons.alternate_email)),
+              Container(
+                color: Colors.blue,
+                child: Text('Column'),
+                padding: EdgeInsets.all(20.0),
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
